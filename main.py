fbbd86f1f39575d3b8b183bf341e315f80a05e06
Devika Suresh <devikasuresh836@gmail.com>
2025-10-16 10:30:18 +0530

    Update warning

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from app.routes import artist_routes,booking_routes,movie_routes,show_routes,use
 from app.config.postgres_config import engine
 from app.models.postgres import Base
 
+import warnings
+warnings.filterwarnings(
+    "ignore",
+    message="Valid config keys have changed in V2:",
+    category=UserWarning
+)
+
+
+
 Base.metadata.create_all(bind = engine)
 
 app = FastAPI(
